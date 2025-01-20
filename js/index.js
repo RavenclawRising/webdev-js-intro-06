fc2c5089ad0318bf603bed672f6aef6ea10506a3
unknown <Lauradu@me.com>
2025-01-20 17:07:32 -0500

    Modified README file, assignment.js, and index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,55 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+
+//for loop
+for (let i = 2; i <= 8; i++) {
+    console.log(i);
+}
+
+// Output: 
+2 
+3
+4 
+5 
+6 
+7 
+8
+
+
+//while loop
+let i = 2;
+while (i < 9) {
+    console.log(i);
+    i++;
+}
+
+//Output
+2
+3
+4
+5
+6
+7
+<--8
+
+
+
+//Do while loop
+let i = 2;
+do {
+    console.log(i);
+    i++;
+} while (i < 9);
+
+//Output
+2
+3
+4
+5
+6
+7
+<--8
+
+*/
